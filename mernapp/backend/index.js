226ed2a9bde00339e1b576cddfc52e78fdb9fab9
mahilrashid09 <mahilrashid09@gmail.com>
2024-01-13 22:51:55 +0530

    first commit

--- mernapp/backend/index.js
+++ mernapp/backend/index.js
@@ -12,14 +12,14 @@ const path = require("path");
 const bookmarkRoutes = require('./Routes/BookmarkRoutes');
 
 app.use(cors());
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  next();
-});
+// app.use((req, res, next) => {
+//   res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+//   res.header(
+//     "Access-Control-Allow-Headers",
+//     "Origin, X-Requested-With, Content-Type, Accept"
+//   );
+//   next();
+// });
 app.use(bodyParser.json());
 app.use(express.json())
 app.use('/api', require("./Routes/createUser"));
